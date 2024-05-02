f194b8b39d0a755273dc9c3987f8534ee1b9cbaa
kaiboy333 <hirohiro20001217@gmail.com>
2024-05-03 01:59:29 +0900

    [feat] : Slack認証ボタン追加
    [fix] : ファイル入れ忘れ
    
    [change] : Slackボタンを有効なURLに設定
    
    [fix] : ルートの入れ忘れ
    
    [fix] : oauthがindexになっていたのを修正
    
    [test] : テスト
    
    Revert "[test] : テスト"
    
    This reverts commit 58131146121fa3212babed84f2a6bb17ac07db9a.
    
    [fix] : urlと関数名があっていなかったのを修正
    
    [test] : 接続テスト
    
    [fix] : セミコロン入れ忘れ
    
    [fix] : シングルクォーテーションの付け忘れ

---
+++ app/Http/Controllers/OAuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OAuthController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('index');
+    }
+
+    public function auth(Request $request)
+    {
+        \Log::info("request: ${request}");
+
+        $code = $request->input('code');
+        $state = $request->input('state');
+
+        \Log::info("code: ${code}");
+        \Log::info("state: ${state}");
+
+        if ($request->filled('error'))
+        {
+            return response('slack returned error response.', 500);
+        }
+
+        $client = new \GuzzleHttp\Client();
+
+        $params = [
+            'form_params' => [
+                'client_id' => config('slack.client_id'),
+                'client_secret' => config('slack.client_secret'),
+                'code' => $code,
+                'redirect_uri' => config('redirect_uri'),    
+            ]
+        ];
+
+        // HTTP POSTリクエスト
+        $oauth_response = $client->request('POST', config('oauth_access_api_uri'), $params);
+        $body = json_decode($oauth_response->getBody(), true);
+
+        if (!$body['ok'])
+        {
+            return response('oauth returned error response.', 500);
+        }
+
+        $access_token = $body['access_token'];
+        $user_id = $body['authed_user']['id'];
+
+        \Log::info("access_token: ${access_token}");
+        \Log::info("user_id: ${user_id}");
+
+        return "ok";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// 追加
+Route::get('/index', 'App\Http\Controllers\OAuthController@index');
+Route::get('/auth', 'App\Http\Controllers\OAuthController@auth');
